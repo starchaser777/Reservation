c00567cea0dbf9e19e74cc3baa85a3e95a42ece2
starchaser777 <les980730@gmail.com>
2024-04-18 16:38:14 +0900

    직접 풀어보기 6-1 첫번째 문제 완료

--- app/src/main/java/kr/ac/kopo/reservation2004/MainActivity.kt
+++ app/src/main/java/kr/ac/kopo/reservation2004/MainActivity.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.CalendarView
 import android.widget.Chronometer
+import android.widget.DatePicker
 import android.widget.RadioGroup
 import android.widget.RadioGroup.OnCheckedChangeListener
 import android.widget.TextView
@@ -19,7 +20,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var btnStart : Button
     lateinit var btnDone : Button
     lateinit var rg : RadioGroup
-    lateinit var calendar : CalendarView
+    lateinit var datePick : DatePicker
     lateinit var timePick : TimePicker
     lateinit var textResult : TextView
     var selectedYear : Int = 0
@@ -34,11 +35,11 @@ class MainActivity : AppCompatActivity() {
         btnStart = findViewById<Button>(R.id.btnStart)
         btnDone = findViewById<Button>(R.id.btnDone)
         rg = findViewById<RadioGroup>(R.id.rg)
-        calendar = findViewById<CalendarView>(R.id.calendar)
+        datePick = findViewById<DatePicker>(R.id.datePick)
         timePick = findViewById<TimePicker>(R.id.timePick)
         textResult = findViewById<TextView>(R.id.textResult)
 
-        calendar.visibility = View.INVISIBLE
+        datePick.visibility = View.INVISIBLE
         timePick.visibility = View.INVISIBLE
 
         rg.setOnCheckedChangeListener(rgListener)
@@ -51,23 +52,27 @@ class MainActivity : AppCompatActivity() {
         btnDone.setOnClickListener {
             chrono.stop()
             chrono.setTextColor(Color.CYAN)
+            selectedYear = datePick.year
+            selectedMonth = datePick.month + 1
+            selectedDay = datePick.dayOfMonth
+
             textResult.setText("" + selectedYear + "년 " + selectedMonth + "월 " + selectedDay + "일 ")
             textResult.append("" + timePick.currentHour + "시 ")
             textResult.append("" + timePick.currentMinute + "분")
         }
 
-        calendar.setOnDateChangeListener { view, year, month, dayOfMonth ->
-            selectedYear = year
-            selectedMonth = month + 1
-            selectedDay = dayOfMonth
-        }
+//        calendar.setOnDateChangeListener { view, year, month, dayOfMonth ->
+//            selectedYear = year
+//            selectedMonth = month + 1
+//            selectedDay = dayOfMonth
+//        }
     }
 
     var rgListener = OnCheckedChangeListener { group, checkedId ->
-        calendar.visibility = View.INVISIBLE
+        datePick.visibility = View.INVISIBLE
         timePick.visibility = View.INVISIBLE
         when(rg.checkedRadioButtonId) {
-            R.id.rbDate -> calendar.visibility = View.VISIBLE
+            R.id.rbDate -> datePick.visibility = View.VISIBLE
             R.id.rbTime -> timePick.visibility = View.VISIBLE
         }
     }
